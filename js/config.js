735a5bd038bf9442e5dd1841bf274d2ad3711e99
ekilhan <erhankenanilhan584@gmail.com>
2025-12-12 22:10:13 +0300

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_XEqNj7WVh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '74h13koe083fbs9hd75r35pi43', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
