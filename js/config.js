3ce0ab57791b3af4d899e1d8e37244a3634468b1
ekilhan <erhankenanilhan584@gmail.com>
2025-12-12 22:16:13 +0300

    Update Cognito userPoolId and userPoolClientId

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_XEqNj7WVh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '74h13koe083fbs9hd75r35pi43', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_DGyZRAIv8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 't0f3oh1bnj67d727jn42cttrp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
